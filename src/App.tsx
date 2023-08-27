3044426d6c0aa3238b13aa05a8846fe89d0c696c
SirCodr <jotarestrepo01@gmail.com>
2023-08-27 12:58:48 -0500

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App () {
+  return <div>Finanzapp</div>
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+  plugins: [react()],
+})
