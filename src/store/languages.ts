f726df4500d9df7ab922bd66937492e965ba1c16
SirCodr <jotadeveloper00@gmail.com>
2023-09-01 12:02:44 -0500

    Persist languages store statein local storage

--- src/store/languages.ts
+++ src/store/languages.ts
@@ -1,14 +1,19 @@
 import type { LanguageType } from '@src/types/language'
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface LanguagesState {
   languages: LanguageType[]
   setLanguages: (languages: LanguageType[]) => void
 }
 
-export const useLanguagesStore = create<LanguagesState>()((set) => ({
-  languages: [],
-  setLanguages: (languages) => {
-    set(() => ({ languages }))
-  }
-}))
+export const useLanguagesStore = create<LanguagesState>()(
+  persist((set) => ({
+    languages: [],
+    setLanguages: (languages) => {
+      set(() => ({ languages }))
+    }
+  }), {
+    name: 'languages-storage'
+  })
+)
