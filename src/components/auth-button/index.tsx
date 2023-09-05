a607ac3c160af94a110e75a4f25fc63afacaab41
SirCodr <jotadeveloper00@gmail.com>
2023-09-04 23:12:07 -0500

    Extend tailwind colors

--- src/components/auth-button/index.tsx
+++ src/components/auth-button/index.tsx
@@ -16,7 +16,7 @@ const AuthButton = ({ userLogged }: Props) => {
       <button
         type='button'
         onClick={signInWithGoogle}
-        className='text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2'
+        className='text-white bg-blue-primary hover:bg-blue-primary/90 focus:ring-4 focus:outline-none focus:ring-blue-primary/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-blue-primary/55 mr-2 mb-2'
       >
         <GoogleIcon />
         {t('signIn.with')} Google
@@ -28,7 +28,7 @@ const AuthButton = ({ userLogged }: Props) => {
     <button
       type='button'
       onClick={handleSignOut}
-      className='text-white bg-[#E70D0D] hover:bg-[#E70D0D]/90 focus:ring-4 focus:outline-none focus:ring-[#E70D0D]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#E70D0D]/55 mr-2 mb-2'
+      className='text-white bg-red-primary hover:bg-red-primary/90 focus:ring-4 focus:outline-none focus:ring-red-primary/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-red-primary/55 mr-2 mb-2'
     >
       <GoogleIcon />
       {t('signOut')}
