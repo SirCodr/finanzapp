248fbd6e4862f1ad751ad6ba8fe8648d796a7ab5
SirCodr <jotadeveloper00@gmail.com>
2023-09-27 08:48:53 -0500

    Refactor route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React, { Suspense } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import ProtectedRoute from './components/routes/ProtectedRoute'
 import Login from './pages/login'
 
-const Welcome = React.lazy(async () => await import('./pages/Welcome'))
 const TransactionsPage = React.lazy(async () => await import('./pages/transactions'))
 const CreateTransactionPage = React.lazy(async () => await import('./pages/transactions/Create'))
 const PageNotFound = React.lazy(async () => await import('./components/PageNotFound'))
@@ -15,7 +14,7 @@ function App () {
     <Suspense fallback={<div>{t('loading.suspense')}</div>}>
         <Routes>
           <Route path='/' element={<ProtectedRoute />}>
-            <Route path='' element={<Welcome />} />
+            <Route path='' element={<Navigate to='/transactions' />} />
             <Route path='transactions' element={<TransactionsPage />} />
             <Route path='transactions/create' element={<CreateTransactionPage />} />
             <Route path='*' element={<PageNotFound />} />
